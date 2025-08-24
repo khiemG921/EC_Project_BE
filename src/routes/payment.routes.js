ec26fe7d912cd2d6196a3f65167fe359476480aa
pililover <118993806+pililover@users.noreply.github.com>
2025-08-24 17:56:19 +0700

    Update payment.routes.js

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const verifySession = require('../middleware/auth.middle').verifyToken; // hoặc verifyToken
 const { createPaypalOrder, capturePaypalOrder, createMoMoOrder, createZaloPayOrder } = require('../controllers/payment.controller');
+const { verifyToken } = require('../middleware/auth.middle');
 
 const router = express.Router();
-router.post('/payment/paypal/create-order', verifySession, createPaypalOrder);
-router.post('/payment/paypal/capture-order', verifySession, capturePaypalOrder);
+router.post('/payment/paypal/create-order', verifyToken, createPaypalOrder);
+router.post('/payment/paypal/capture-order', verifyToken, capturePaypalOrder);
 
-router.post('/payment/momo/create-order', verifySession, createMoMoOrder);
+router.post('/payment/momo/create-order', verifyToken, createMoMoOrder);
 
-router.post('/payment/zalo/create-order', verifySession, createZaloPayOrder);
+router.post('/payment/zalo/create-order', verifyToken, createZaloPayOrder);
 
 module.exports = router;
